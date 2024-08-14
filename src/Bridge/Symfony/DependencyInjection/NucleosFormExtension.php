443780014d7b0f3727c67852b30df790d9fcc26c
core23 <mail@core23.de>
2024-08-14 17:41:24 +0200

    Fix symfony deprecation

--- src/Bridge/Symfony/DependencyInjection/NucleosFormExtension.php
+++ src/Bridge/Symfony/DependencyInjection/NucleosFormExtension.php
@@ -13,9 +13,9 @@ namespace Nucleos\Form\Bridge\Symfony\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class NucleosFormExtension extends Extension implements PrependExtensionInterface
 {
